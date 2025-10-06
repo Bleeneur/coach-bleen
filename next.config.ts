5e7d3fc11f83a2d18f1ec4690803ac7e7b0ee775
Quentin Lanthier <quentin@mybleen.com>
2025-10-06 21:02:52 +0200

    chore: disable ESLint blocking on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ Empêche Vercel d'arrêter le build sur les erreurs ESLint
+  },
 };
 
 export default nextConfig;
